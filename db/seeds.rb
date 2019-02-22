b6f3d84a4bc03a500649531dc103950f5ddeccaf
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-02-22 11:18:17 +0000

    fix views and add gems for fakers/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
+require 'faker'
 
 # populate ingredients
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-
 ingredients = JSON.parse(open(url).read)
+
 ingredients["drinks"].each do |ingredient|
   i = Ingredient.create(name: ingredient["strIngredient1"])
   puts "creating an ingredient #{i.name}"
 end
 
 # populate cocktails
-cocktails = ["Mojito", "Moscow Mule", "Long Island Iced Tea"]
+cocktails = Array.new(50) { Faker::Zelda.character }
 
 cocktails.each do |cocktail|
   c = Cocktail.create(name: cocktail)
   puts "created a cocktail #{c.name}"
+end
 
-  Dose.create(description: "#{1 + rand(25)}ml", cocktail_id: c.id, ingredient_id: (1 + rand(Ingredient.count)))
-  Dose.create(description: "#{1 + rand(25)}ml", cocktail_id: c.id, ingredient_id: (1 + rand(Ingredient.count)))
-  Dose.create(description: "#{1 + rand(25)}ml", cocktail_id: c.id, ingredient_id: (1 + rand(Ingredient.count)))
+# create some doses for each cocktails
+Cocktail.all.each do |cocktail|
+  # get all the ingredient ids that are available (we'll need this later)
+  ingredient_ids = Ingredient.pluck(:id)
+  
+  # pick a random number of ingredients between 2-5
+  number_of_ingredients = rand(3) + 2
+  
+  # add that many doses
+  number_of_ingredients.times do    
+    # add a random measurement, also assign the cocktail and a random ingredient
+    Dose.create(description: Faker::Measurement.metric_volume, cocktail: cocktail, ingredient: Ingredient.find(ingredient_ids.sample))
+    puts "added a dose to #{cocktail.name}"
+  end
 end
