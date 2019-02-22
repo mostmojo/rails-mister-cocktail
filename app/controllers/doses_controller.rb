a22aecd4e6aa9789cd2f9cde8537d9a1ffe84d57
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-02-22 11:03:42 +0000

    fixed controllers and seed files with JSON:

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,30 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Dose.find(params[:id]).cocktail
+    Dose.delete(params[:id])
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+# populate ingredients
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "creating an ingredient #{i.name}"
+end
+
+# populate cocktails
+cocktails = ["Mojito", "Moscow Mule", "Long Island Iced Tea"]
+
+cocktails.each do |cocktail|
+  c = Cocktail.create(name: cocktail)
+  puts "created a cocktail #{c.name}"
+
+  Dose.create(description: "#{1 + rand(25)}ml", cocktail_id: c.id, ingredient_id: (1 + rand(Ingredient.count)))
+  Dose.create(description: "#{1 + rand(25)}ml", cocktail_id: c.id, ingredient_id: (1 + rand(Ingredient.count)))
+  Dose.create(description: "#{1 + rand(25)}ml", cocktail_id: c.id, ingredient_id: (1 + rand(Ingredient.count)))
+end
